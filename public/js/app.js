32f6cc320131d3643d65822a37dd403141da408f
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2019-08-25 17:23:00 +0530

    added front end style and serve data to server

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,24 @@
 console.log('Client side js is loaded');
 
-fetch('http://puzzle.mead.io/puzzle').then(response => {
-  response.json().then(data => {
-    console.log(data);
-  });
-});
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
 
 weatherForm.addEventListener('submit', e => {
   e.preventDefault();
   const location = search.value;
 
+  messageOne.textContent = 'Loading...';
+  messageTwo.textContent = '';
+
   fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
     response.json().then(({ error, forecast, location } = {}) => {
       if (error) {
-        console.log('Error', error);
+        messageOne.textContent = error;
       } else {
-        console.log(`Forecase for ${location}:  ${forecast}`);
+        messageOne.textContent = location;
+        messageTwo.textContent = forecast;
       }
     });
   });
