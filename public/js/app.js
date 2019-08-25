6e01c72659d088cbbbb33ceec1c61a1a404275c1
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2019-08-25 17:08:27 +0530

    set up fetch api call to server to get data from our express server

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,25 @@
 console.log('Client side js is loaded');
+
+fetch('http://puzzle.mead.io/puzzle').then(response => {
+  response.json().then(data => {
+    console.log(data);
+  });
+});
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+weatherForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const location = search.value;
+
+  fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
+    response.json().then(({ error, forecast, location } = {}) => {
+      if (error) {
+        console.log('Error', error);
+      } else {
+        console.log(`Forecase for ${location}:  ${forecast}`);
+      }
+    });
+  });
+});
